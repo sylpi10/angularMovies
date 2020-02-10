047fd7bb368f2286aad599e298c23d4b279dce3f
syl pi <syl.pillet@hotmail.fr>
2020-02-10 11:43:04 +0100

    create few elts in index

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+@Component({                                //
+  selector: 'app-root',                     // decorateur
+  templateUrl: './app.component.html',      //
+  styleUrls: ['./app.component.scss']       // 
 })
 export class AppComponent {
-  title = 'movies';
+  public title: string = 'Amazing Movies';
+
+  public defaultCountry: string = 'US';
+
+  public movies: any[] = [
+    {
+      title: 'Joker',
+      year: 2019,
+      country: this.defaultCountry
+    },
+    {
+      title: "Avengers",
+      year: 2012,
+      country: this.defaultCountry
+    },
+    {
+      title: "Once upon a time in the weeeest",
+      year: 1972,
+      country: 'IT'
+    }
+  ];
+
+  /**
+   * toggleCountry
+   */
+  public toggleCountry(): void {
+    // this.defaultCountry = 'US' ? 'IT' : 'US';
+
+    if (this.defaultCountry == 'US') {
+      this.defaultCountry = 'IT';
+    } else {
+      this.defaultCountry = 'US'
+    }
+  }
+
 }
