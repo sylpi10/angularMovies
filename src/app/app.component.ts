2398d8f64a5b66caa19293e4acdc951d7713d575
syl pi <syl.pillet@hotmail.fr>
2020-02-10 16:32:50 +0100

    create select list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,28 +8,55 @@ import { Component } from '@angular/core';
 export class AppComponent {
   public title: string = 'Amazing Movies';
 
-  public defaultCountry: string = 'US';
+  public defaultCountry: string = 'all';
 
   public movies: any[] = [
     {
       title: 'Joker',
       year: 2019,
-      country: this.defaultCountry,
+      code: 'US',
+      country: 'United States',
       shown: true
     },
     {
       title: "Avengers",
       year: 2012,
-      country: this.defaultCountry,
+      code: 'US',
+      country: 'United States',
       shown: true
     },
     {
       title: "Once upon a time in the west",
       year: 1972,
-      country: 'IT',
+      code: 'IT',
+      country: 'Italy',
+      shown: false
+    },
+    {
+      title: "rabbi jacob",
+      year: 1969,
+      code: 'FR',
+      country: 'France',
       shown: false
     }
   ];
+
+  public countries: string[] = [];
+
+  public constructor() {
+    this.movies.forEach(movie => {
+      if (!this.countries.includes(movie.code)) {
+        this.countries.push(movie.code);
+      }
+    });
+  }
+
+  // public countries2: Set<string> = new Set();
+  //   public constructor() {
+  //     this.movies.forEach(movie =>{
+  //       this.countries2.add(movie.code);
+  //     })
+  //   }
 
   /**
    * toggleCountry
