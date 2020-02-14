78b89b3a4bf7ceedb1c5d6aab40db4a302c77aa7
syl pi <syl.pillet@hotmail.fr>
2020-02-14 15:02:23 +0100

    add route and paramMap

--- src/app/pages/movie/movie.component.ts
+++ src/app/pages/movie/movie.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-movie',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe((paramMap: any) => {
+      console.log(`params : ${paramMap.params.id}`);
+    });
   }
 
 }
