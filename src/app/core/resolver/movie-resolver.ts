3815cd5e20daaa35c64e2bcd9912ea823db364d1
syl pi <syl.pillet@hotmail.fr>
2020-02-18 11:59:27 +0100

    add resolver

---
+++ src/app/core/resolver/movie-resolver.ts
@@ -0,0 +1,17 @@
+import { Resolve } from '@angular/router';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class MovieResolver implements Resolve<any> {
+   public  resolve(
+       route: import("@angular/router").ActivatedRouteSnapshot, 
+       state: import("@angular/router").RouterStateSnapshot
+       ) {
+        console.log(`hello resolver`);
+        // throw new Error("Method not implemented.");
+    }
+    
+}
+    
